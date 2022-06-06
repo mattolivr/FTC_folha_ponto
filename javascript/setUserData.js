efc13b4a17d26f093a1958fe7743ddaa44cf1012
Matheus de Oliveira <matheusdeoliveira310@gmail.com>
2022-06-06 12:13:47 -0300

    Sistema de erros e verificação do campo nome

--- javascript/setUserData.js
+++ javascript/setUserData.js
@@ -1,3 +1,5 @@
+// Funções para coletar dados de ENTRADA
+
 function getName() {
     const input = document.getElementById("input_name")
     return input.value
@@ -13,13 +15,13 @@ function getDate() {
     return [time.getDate(), (time.getMonth() + 1), time.getFullYear()].join("/")
 }
 
-function getEmployeeArray() {
-    if (localStorage.getItem("signTimeSheet") == null) {
-        return new Array()
-    } else {
-        return JSON.parse(localStorage.getItem("signTimeSheet"))
-    }
+function isNameInputValid() {
+    const input = document.getElementById("input_name")
+    console.log(input.value)
+    return (input.value != "")
 }
+
+// Construtor de objeto funcionário
 
 function setNewEmployee() {
     return {
@@ -29,28 +31,67 @@ function setNewEmployee() {
     }
 }
 
-function showEmployeeData(employee) {
-    const data = document.getElementById("user_data")
-    data.innerHTML = `<strong>Enviado com sucesso!</strong> ${employee.name.toUpperCase()} - ${employee.date} ${employee.time}`
-    data.style.display = "block";
+// Array de objetos de funcionários
+
+function getEmployeeArray() {
+    if (localStorage.getItem("signTimeSheet") == null) {
+        return new Array()
+    } else {
+        return JSON.parse(localStorage.getItem("signTimeSheet"))
+    }
 }
+
+// Inserir objeto no array e salvar no LocalStorage
 
 function setSheet() {
     let FolhaPonto = getEmployeeArray()
     let employee = setNewEmployee()
 
-    FolhaPonto.push(employee)
-    localStorage.setItem("signTimeSheet", JSON.stringify(FolhaPonto))
-    
-    showEmployeeData(employee)
+    resetAlerts()
+
+    if(isNameInputValid()){
+        FolhaPonto.push(employee)
+        localStorage.setItem("signTimeSheet", JSON.stringify(FolhaPonto))
+        
+        showValidAlert(employee)
+    } else {
+        showErrorAlert("Campo nome não pode estar vazio")
+    }
 }
+
+// Mostrar alerta de sucesso na tela
+
+function showValidAlert(employeeData) {
+    const validContainer = document.getElementById("user_data")
+    validContainer.innerHTML = `<strong>Enviado com sucesso!</strong> ${employeeData.name.toUpperCase()} - ${employeeData.date} ${employeeData.time}`
+    validContainer.style.display = "block"
+}
+
+// Mostra mensagem de erro
+
+function showErrorAlert(error) {
+    const errorContainer = document.getElementById("error_msg")
+    errorContainer.innerHTML = `<strong>${error}</strong>`
+    errorContainer.style.display = "block"
+}
+
+// Reseta mensagens
+
+function resetAlerts() {
+    const validContainer = document.getElementById("user_data")
+    const errorContainer = document.getElementById("error_msg")
+
+    validContainer.style.display = "none"
+    errorContainer.style.display = "none"
+}
+
+// Transforma o array de objetos em um arquivo JSON
 
 function saveToFile() {
     let FolhaPonto = getEmployeeArray()
     let jsonObjectAsString = JSON.stringify(FolhaPonto)
   
     let blob = new Blob([jsonObjectAsString], {
-    //   type: 'application/json'
       type: 'octet/stream'
     })
   
@@ -58,4 +99,4 @@ function saveToFile() {
     anchor.download = "signTimeSheet.json";
     anchor.href = window.URL.createObjectURL(blob);
     anchor.click();
-  }
+}
